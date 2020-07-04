c22dadab075797a14459b0f8871285a34c2a8ff1
Oliver Kossin <thecadiens@gmail.com>
2020-07-04 13:50:51 +0200

    Add Readme and Change Namespaces

--- SuluImportExportBundle.php
+++ SuluImportExportBundle.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace App\Bundles\SuluImportExportBundle;
+namespace TheCadien\Bundle\SuluImportExportBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
